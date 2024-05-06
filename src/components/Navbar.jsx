6d585f5c4150f069b4a451b189094352e3d74010
shivam sharma <ss6841264@gmail.com>
2024-05-06 23:01:58 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 
         <NavLink to="/">
           <div className="ml-5">
-          <img src="../logo.png" className="h-14" alt="image"/>
+          <img src="../logo.png" className="h-14"/>
           </div>
         </NavLink>
 
